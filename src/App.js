b768f347cf97cfe28cec2a064a9a6141680a96fb
Carlos Leon <cleonve@icloud.com>
2021-08-11 20:17:36 -0500

    Add react-route to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Landing from './pages/landing';
+import BankList from './pages/bankList';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/navbar';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+
+      <Navbar />
+      
+      <Router>
+          <Switch>
+
+            <Route path="/bankList">
+              <BankList />
+            </Route>
+
+            <Route path="/">
+              <Landing />
+            </Route>
+
+          </Switch>
+      </Router>
+      
+    </Fragment>
   );
 }
 
